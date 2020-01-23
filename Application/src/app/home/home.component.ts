66d4475c1973e721b17966e0e36326d608fbae10
AnanyaRao <38326912+AnanyaRao@users.noreply.github.com>
2020-01-23 17:52:12 +0530

    Add files via upload

---
+++ Application/src/app/home/home.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import {NgModule} from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  sample:any = "Variable from home component";
+    data ="Before Click"
+    bt=true;
+    condition: boolean;
+    data1=""
+    returnMsg=""
+    showMsg=false
+    servers = ["Server1","Server2","Server3"];
+    myServer:string;
+    temperature;
+    //color;
+  constructor() { }
+
+  ngOnInit() {
+   // this.temperature = Math.random();
+    if(Math.random()>0.5){
+       this.temperature='hot'
+    }else{
+      this.temperature='cold'
+    }
+  }
+  show(){
+    this.showMsg = !this.showMsg;
+  }
+
+  addElement(){
+    this.servers.push(this.myServer);
+  }
+  getColor(){
+    if(this.temperature=='hot'){
+      return 'red';
+    }else{
+      return 'blue';
+    }
+  }
+  display(){
+    this.data="After Click";
+    this.bt=false;
+
+    if(this.data1.length>=5){
+      this.returnMsg="Letters greater than 5";
+    }
+    else{
+      this.returnMsg="Letters lesser than 5";
+    }
+    return this.returnMsg;
+  }
+ 
+}
