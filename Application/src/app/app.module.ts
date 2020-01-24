0ae089786647dc8c9c44cc01f0bac4c49dd190b8
AnanyaRao <38326912+AnanyaRao@users.noreply.github.com>
2020-01-24 12:27:05 +0530

    Update app.module.ts

--- Application/src/app/app.module.ts
+++ Application/src/app/app.module.ts
@@ -5,18 +5,23 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { ServiceService } from './service.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DatabindingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    ServiceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
