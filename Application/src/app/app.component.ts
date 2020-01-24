53e3ec8057ea7edcef6baccb617373c512c7eee6
AnanyaRao <38326912+AnanyaRao@users.noreply.github.com>
2020-01-24 17:34:50 +0530

    Update app.component.ts

--- Application/src/app/app.component.ts
+++ Application/src/app/app.component.ts
@@ -9,8 +9,13 @@ export class AppComponent {
   title = 'Application';
   courseName;
   colorname;
+  totalPrice;
   register(course){
 this.courseName=course;
+  }
+  setTotalPrice(totalPrice){
+    this.totalPrice=totalPrice;
+
   }
 colorSet(){
   this.colorname=this.courseName;
